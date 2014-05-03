a52ec1e7d613ebd13537612413c205a236cd1369
Nils Olsson <nils.olsson@arcticblue.se>
2014-05-03 22:47:57 +0200

    Update efsm builders

--- src/main/java/org/graphwalker/core/model/efsm/Edge.java
+++ src/main/java/org/graphwalker/core/model/efsm/Edge.java
@@ -1,7 +1,46 @@
 package org.graphwalker.core.model.efsm;
 
+import org.graphwalker.core.model.ModelBuilder;
+
 /**
  * @author Nils Olsson
  */
-public class Edge {
+public final class Edge {
+
+    private final Vertex source;
+    private final Vertex target;
+
+    private Edge(Builder builder) {
+        this.source = builder.source.build();
+        this.target = builder.target.build();
+    }
+
+    public Vertex getSource() {
+        return source;
+    }
+
+    public Vertex getTarget() {
+        return target;
+    }
+
+    public static class Builder implements ModelBuilder<Edge> {
+
+        public Vertex.Builder source;
+        public Vertex.Builder target;
+
+        public Builder source(Vertex.Builder vertex) {
+            this.source = vertex;
+            return this;
+        }
+
+        public Builder target(Vertex.Builder vertex) {
+            this.target = vertex;
+            return this;
+        }
+
+        @Override
+        public Edge build() {
+            return new Edge(this);
+        }
+    }
 }
--- src/test/java/org/graphwalker/core/model/ModelBuilderTest.java
+++ src/test/java/org/graphwalker/core/model/ModelBuilderTest.java
@@ -1,8 +1,11 @@
 package org.graphwalker.core.model;
 
+import org.graphwalker.core.model.efsm.Edge;
+import org.graphwalker.core.model.efsm.Vertex;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.notNullValue;
 
 /**
@@ -12,7 +15,13 @@ public class ModelBuilderTest {
 
     @Test
     public void build() {
-        EFSM efsm = new EFSM.Builder().build();
+        Vertex.Builder vertex1 = new Vertex.Builder();
+        Vertex.Builder vertex2 = new Vertex.Builder();
+        EFSM efsm = new EFSM.Builder()
+                .add(new Edge.Builder().source(vertex1).target(vertex2))
+                .build();
         Assert.assertThat(efsm, notNullValue());
+        Assert.assertThat(efsm.getEdges().size(), is(1));
+        Assert.assertThat(efsm.getVertices().size(), is(2));
     }
 }
