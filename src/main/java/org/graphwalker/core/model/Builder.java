d872b1d413df2ae88502c6c1beb78482a5e7851b
Nils Olsson <nils.olsson@arcticblue.se>
2014-05-04 13:30:07 +0200

    Refactor and added marker interface for model and element

--- src/main/java/org/graphwalker/core/model/Builder.java
+++ src/main/java/org/graphwalker/core/model/Builder.java
@@ -26,21 +26,10 @@ package org.graphwalker.core.model;
  * #L%
  */
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-
 /**
  * @author Nils Olsson
  */
-public final class ModelBuilderSet<T extends ModelBuilder<E>, E> extends HashSet<T> implements ModelBuilder<List<E>> {
-
-    @Override
-    public List<E> build() {
-        List<E> elements = new ArrayList<>();
-        for (T builder: this) {
-            elements.add(builder.build());
-        }
-        return elements;
-    }
+public interface Builder<T> {
+    T build();
 }
+
--- src/test/java/org/graphwalker/core/model/ModelBuilderTest.java
+++ src/test/java/org/graphwalker/core/model/ModelBuilderTest.java
@@ -28,6 +28,8 @@ package org.graphwalker.core.model;
 
 import org.graphwalker.core.model.efsm.Edge;
 import org.graphwalker.core.model.efsm.Vertex;
+import static org.graphwalker.core.model.efsm.Edge.EdgeBuilder;
+import static org.graphwalker.core.model.efsm.Vertex.VertexBuilder;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -42,26 +44,26 @@ public class ModelBuilderTest {
 
     @Test
     public void buildVertex() {
-        Vertex vertex = new Vertex.Builder().build();
+        Vertex vertex = new VertexBuilder().build();
         Assert.assertThat(vertex, notNullValue());
     }
 
     @Test
     public void buildEdge() {
-        Vertex.Builder vertex1 = new Vertex.Builder();
-        Vertex.Builder vertex2 = new Vertex.Builder();
-        Edge edge = new Edge.Builder().setSource(vertex1).setTarget(vertex2).build();
+        VertexBuilder vertex1 = new VertexBuilder();
+        VertexBuilder vertex2 = new VertexBuilder();
+        Edge edge = new EdgeBuilder().setSourceVertex(vertex1).setTargetVertex(vertex2).build();
         Assert.assertThat(edge, notNullValue());
-        Assert.assertThat(edge.getSource(), notNullValue());
-        Assert.assertThat(edge.getTarget(), notNullValue());
+        Assert.assertThat(edge.getSourceVertex(), notNullValue());
+        Assert.assertThat(edge.getTargetVertex(), notNullValue());
     }
 
     @Test
     public void buildEFSM() {
-        Vertex.Builder vertex1 = new Vertex.Builder();
-        Vertex.Builder vertex2 = new Vertex.Builder();
+        VertexBuilder vertex1 = new VertexBuilder();
+        VertexBuilder vertex2 = new VertexBuilder();
         EFSM efsm = new EFSM.Builder()
-                .add(new Edge.Builder().setSource(vertex1).setTarget(vertex2))
+                .add(new EdgeBuilder().setSourceVertex(vertex1).setTargetVertex(vertex2))
                 .build();
         Assert.assertThat(efsm, notNullValue());
         Assert.assertThat(efsm.getEdges().size(), is(1));
@@ -71,14 +73,14 @@ public class ModelBuilderTest {
 
     @Test
     public void updateBuilder() {
-        Vertex.Builder vertex1 = new Vertex.Builder();
-        Vertex.Builder vertex2 = new Vertex.Builder();
-        Edge.Builder edge1 = new Edge.Builder().setSource(vertex1).setTarget(vertex2);
+        VertexBuilder vertex1 = new VertexBuilder();
+        VertexBuilder vertex2 = new VertexBuilder();
+        EdgeBuilder edge1 = new EdgeBuilder().setSourceVertex(vertex1).setTargetVertex(vertex2);
         EFSM.Builder efsm = new EFSM.Builder().add(edge1);
         Assert.assertThat(efsm.build(), notNullValue());
         Assert.assertThat(efsm.build().getEdges().size(), is(1));
         Assert.assertThat(efsm.build().getVertices().size(), is(2));
-        Edge.Builder edge2 = new Edge.Builder().setSource(vertex1).setTarget(vertex2);
+        EdgeBuilder edge2 = new EdgeBuilder().setSourceVertex(vertex1).setTargetVertex(vertex2);
         efsm.add(edge2);
         Assert.assertThat(efsm.build(), notNullValue());
         Assert.assertThat(efsm.build().getEdges().size(), is(2));
@@ -87,7 +89,7 @@ public class ModelBuilderTest {
 
     @Test
     public void singleVertex() {
-        EFSM efsm = new EFSM.Builder().add(new Vertex.Builder()).build();
+        EFSM efsm = new EFSM.Builder().add(new VertexBuilder()).build();
         Assert.assertThat(efsm, notNullValue());
         Assert.assertThat(efsm.getEdges().size(), is(0));
         Assert.assertThat(efsm.getVertices().size(), is(1));
