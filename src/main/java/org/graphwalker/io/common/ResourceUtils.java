4addde37d4100b47eddea819ebd69e172994dce9
Nils Olsson <nils.olsson@arcticblue.se>
2014-05-09 13:06:42 +0200

    Started with a ANTLR4 parser for the graphml labels

---
+++ src/main/java/org/graphwalker/io/common/ResourceUtils.java
@@ -0,0 +1,92 @@
+package org.graphwalker.io.common;
+
+/*
+ * #%L
+ * GraphWalker Input/Output
+ * %%
+ * Copyright (C) 2011 - 2014 GraphWalker
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.lang.String;
+import java.lang.Thread;
+import java.net.URL;
+
+/**
+ * @author Nils Olsson
+ */
+public final class ResourceUtils {
+
+    private ResourceUtils() {
+    }
+
+    public static File getResourceAsFile(final String filename) {
+        File file = createFile(filename);
+        if (file.exists()) {
+            return file;
+        } else {
+            URL resource = ResourceUtils.class.getResource(filename);
+            if (null == resource) {
+                resource = Thread.currentThread().getContextClassLoader().getResource(filename);
+            }
+            if (null != resource) {
+                return new File(resource.getFile());
+            }
+            throw new ResourceException();
+        }
+    }
+
+    public static InputStream getResourceAsStream(final String filename) {
+        File file = createFile(filename);
+        if (file.exists()) {
+            try {
+                return new FileInputStream(file);
+            } catch (FileNotFoundException e) {
+                throw new ResourceException();
+            }
+        } else {
+            InputStream resource = ResourceUtils.class.getResourceAsStream(filename);
+            if (null == resource) {
+                resource = Thread.currentThread().getContextClassLoader().getResourceAsStream(filename);
+            }
+            if (null != resource) {
+                return resource;
+            }
+            throw new ResourceException();
+        }
+    }
+
+    private static String[] splitPath(String filename) {
+        return filename.split("[\\\\/]");
+    }
+
+    private static File createFile(String filename) {
+        File createdFile = null;
+        for (String part : splitPath(filename)) {
+            createdFile = new File(createdFile, part);
+        }
+        return createdFile;
+    }
+}
---
+++ src/test/java/org/graphwalker/io/factory/GraphMLModelFactoryTest.java
@@ -0,0 +1,83 @@
+package org.graphwalker.io.factory;
+
+/*
+ * #%L
+ * GraphWalker Input/Output
+ * %%
+ * Copyright (C) 2011 - 2014 GraphWalker
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+import org.graphwalker.core.model.Model;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+
+/**
+ * @author Nils Olsson
+ */
+public class GraphMLModelFactoryTest {
+
+    @Test(expected = ModelFactoryException.class)
+    public void fileDoesNotExistsOnFileSystem() {
+        GraphMLModelFactory factory = new GraphMLModelFactory();
+        Model model = factory.create("graphml/LKHDIODSOSUBD.graphml");
+    }
+/*
+    @Test
+    public void fileExistsOnFileSystem() {
+        GraphMLModelFactory factory = new GraphMLModelFactory();
+        Model model = factory.create("src/test/resources/graphml/UC01.graphml");
+        //Assert.assertThat("Number of vertices", model.getVertices().size(), is(8));
+        //Assert.assertThat("Number of edges", model.getEdges().size(), is(12));
+    }
+*/
+
+    @Test
+    public void uc01() {
+        GraphMLModelFactory factory = new GraphMLModelFactory();
+        Model model = factory.create("graphml/UC01.graphml");
+        //Assert.assertThat("Number of vertices", model.getVertices().size(), is(8));
+        //Assert.assertThat("Number of edges", model.getEdges().size(), is(12));
+    }
+/*
+    @Test
+    public void efsmWithReqtags() {
+        GraphMLModelFactory factory = new GraphMLModelFactory();
+        Model model = factory.create("graphml/EFSM_with_REQTAGS.graphml");
+        Assert.assertThat("Number of vertices", model.getVertices().size(), is(7));
+        Assert.assertThat("Number of edges", model.getEdges().size(), is(19));
+    }
+
+    @Test
+    public void guards() {
+        GraphMLModelFactory factory = new GraphMLModelFactory();
+        Model model = factory.create("graphml/Guards.graphml");
+        Assert.assertThat("Number of vertices", model.getVertices().size(), is(3));
+        Assert.assertThat("Number of edges", model.getEdges().size(), is(2));
+        Vertex startVertex = model.getStartVertices().get(0);
+        Edge edgeWithGuard = model.getEdges(startVertex).get(0);
+        Assert.assertEquals(null, edgeWithGuard.getName());
+        Assert.assertNotEquals(edgeWithGuard.getGuard(), new Guard("true"));
+    }
+   */
+}
