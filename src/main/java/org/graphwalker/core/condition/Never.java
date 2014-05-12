bbffdd838dcaac87b16a46709fe3c11a195b1cf0
Nils Olsson <nilols@spotify.com>
2014-05-12 11:13:52 +0200

    Add test for stop conditions

--- src/main/java/org/graphwalker/core/condition/Never.java
+++ src/main/java/org/graphwalker/core/condition/Never.java
@@ -28,56 +28,18 @@ package org.graphwalker.core.condition;
 
 import org.graphwalker.core.machine.ExecutionContext;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 /**
  * @author Nils Olsson
  */
-public final class AlternativeCondition implements StopCondition {
-
-    private List<StopCondition> conditions = new ArrayList<>();
-
-    public void add(StopCondition condition) {
-        this.conditions.add(condition);
-    }
-
-    public List<StopCondition> getStopConditions() {
-        return conditions;
-    }
+public class Never implements StopCondition {
 
     @Override
     public boolean isFulfilled(ExecutionContext context) {
-        for (StopCondition condition : conditions) {
-            if (condition.isFulfilled(context)) {
-                return true;
-            }
-        }
         return false;
     }
 
     @Override
     public double getFulfilment(ExecutionContext context) {
-        double fulfilment = 0;
-        for (StopCondition condition : conditions) {
-            double newFulfilment = condition.getFulfilment(context);
-            if (newFulfilment > fulfilment) {
-                fulfilment = newFulfilment;
-            }
-        }
-        return fulfilment;
-    }
-
-    public String toString() {
-        StringBuilder stringBuilder = new StringBuilder("(");
-        for (Iterator<StopCondition> i = conditions.iterator(); i.hasNext();) {
-            stringBuilder.append(i.next().toString());
-            if (i.hasNext()) {
-                stringBuilder.append(" OR ");
-            }
-        }
-        stringBuilder.append(")");
-        return stringBuilder.toString();
+        return 0;
     }
 }
