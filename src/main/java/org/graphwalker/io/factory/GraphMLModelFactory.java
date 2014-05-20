ecfcfd612199e7c7e0ada42c9e65761c5eca7af0
Nils Olsson <nils.olsson@arcticblue.se>
2014-05-20 18:01:48 +0200

    Simplify parsing graphml files by using the xmlbeans objects

--- src/main/java/org/graphwalker/io/factory/GraphMLModelFactory.java
+++ src/main/java/org/graphwalker/io/factory/GraphMLModelFactory.java
@@ -26,8 +26,17 @@ package org.graphwalker.io.factory;
  * #L%
  */
 
+import com.yworks.xml.graphml.*;
+import com.yworks.xml.graphml.impl.EdgeLabelTypeImpl;
+import com.yworks.xml.graphml.impl.NodeLabelTypeImpl;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.apache.xmlbeans.XmlException;
+import org.apache.xmlbeans.XmlObject;
+import org.graphdrawing.graphml.xmlns.DataType;
+import org.graphdrawing.graphml.xmlns.EdgeType;
+import org.graphdrawing.graphml.xmlns.GraphmlDocument;
+import org.graphdrawing.graphml.xmlns.NodeType;
 import org.graphwalker.core.model.*;
 import org.graphwalker.io.LabelLexer;
 import org.graphwalker.io.LabelParser;
@@ -49,130 +58,83 @@ import static org.graphwalker.io.LabelParser.ActionContext;
  */
 public final class GraphMLModelFactory implements ModelFactory {
 
+    private static final String NAMESPACE = "declare namespace xq='http://graphml.graphdrawing.org/xmlns';";
+
     private Map<String, Vertex> elements = new HashMap<>();
 
     @Override
     public Model create(String file) {
         try {
-            return parse(file);
+            Model model = new Model();
+            GraphmlDocument document = GraphmlDocument.Factory.parse(ResourceUtils.getResourceAsStream(file));
+            addVertices(model, document);
+            addEdges(model, document);
+            return model;
         } catch (Throwable throwable) {
             throw new ModelFactoryException(throwable);
         }
     }
 
-    private Model parse(String file) throws XMLStreamException {
-        Model model = new Model();
-        XMLInputFactory factory = XMLInputFactory.newInstance();
-        XMLStreamReader reader = factory.createXMLStreamReader(ResourceUtils.getResourceAsStream(file));
-        while (reader.hasNext()) {
-            if (reader.next() == START_ELEMENT) {
-                if ("node".equals(reader.getLocalName())) {
-                    Vertex vertex = new Vertex();
-                    elements.put(reader.getAttributeValue(null, "id"), vertex);
-                    parseVertex(vertex, reader);
-                    model.addVertex(vertex);
-                }
-                if ("edge".equals(reader.getLocalName())) {
-                    Edge edge = new Edge();
-                    String source = reader.getAttributeValue(null, "source");
-                    if (null != elements.get(source)) {
-                        edge.setSourceVertex(elements.get(source));
-                    }
-                    String target = reader.getAttributeValue(null, "target");
-                    if (null != elements.get(target)) {
-                        edge.setTargetVertex(elements.get(target));
-                    }
-                    parseEdge(edge, reader);
-                    model.addEdge(edge);
-                }
-            }
-        }
-        return model;
-    }
-
-    private void parseVertex(Vertex vertex, XMLStreamReader reader) throws XMLStreamException {
-        while (reader.hasNext()) {
-            switch (reader.next()) {
-                case START_ELEMENT: {
-                    if ("NodeLabel".equals(reader.getLocalName())) {
-                        parseNodeLabel(vertex, reader, reader.getAttributeValue(0));
-                    }
-                }
-                break;
-                case END_ELEMENT: {
-                    if ("node".equals(reader.getLocalName())) {
-                        return;
+    private void addVertices(Model model, GraphmlDocument document) throws XmlException {
+        for (XmlObject object: document.selectPath(NAMESPACE+"$this/xq:graphml/xq:graph/xq:node")) {
+            if (object instanceof NodeType) {
+                NodeType node = (NodeType)object;
+                for (DataType data: node.getDataArray()) {
+                    if (0 < data.getDomNode().getChildNodes().getLength()) {
+                        ShapeNodeType shape = ShapeNodeDocument.Factory.parse(data.xmlText()).getShapeNode();
+                        StringBuilder label = new StringBuilder();
+                        for (NodeLabelType nodeLabel : shape.getNodeLabelArray()) {
+                            label.append(((NodeLabelTypeImpl) nodeLabel).getStringValue());
+                        }
+                        LabelParser.ParseContext context = parseLabel(label.toString());
+                        Vertex vertex = new Vertex();
+                        if (null != context.name()) {
+                            vertex.setName(context.name().getText());
+                        }
+                        if (null != context.shared() && null != context.shared().Identifier()) {
+                            vertex.setSharedState(context.shared().Identifier().getText());
+                        }
+                        elements.put(node.getId(), vertex);
+                        model.addVertex(vertex);
                     }
                 }
             }
         }
     }
 
-    private void parseNodeLabel(Vertex vertex, XMLStreamReader reader, String id) throws XMLStreamException {
-        StringBuilder builder = new StringBuilder();
-        while (reader.hasNext()) {
-            switch (reader.next()) {
-                case CHARACTERS: {
-                    builder.append(reader.getText());
-                }
-                break;
-                case END_ELEMENT: {
-                    LabelParser.ParseContext context = parseLabel(builder.toString());
-                    if (null != context.name()) {
-                        vertex.setName(context.name().getText());
+    private void addEdges(Model model, GraphmlDocument document) throws XmlException {
+        for (XmlObject object: document.selectPath(NAMESPACE+"$this/xq:graphml/xq:graph/xq:edge")) {
+            if (object instanceof EdgeType) {
+                EdgeType edgeType = (EdgeType)object;
+                for (DataType data: edgeType.getDataArray()) {
+                    if (0 < data.getDomNode().getChildNodes().getLength()) {
+                        PolyLineEdgeType polyLineEdge = PolyLineEdgeDocument.Factory.parse(data.xmlText()).getPolyLineEdge();
+                        StringBuilder label = new StringBuilder();
+                        for (EdgeLabelType edgeLabel : polyLineEdge.getEdgeLabelArray()) {
+                            label.append(((EdgeLabelTypeImpl) edgeLabel).getStringValue());
+                        }
+                        LabelParser.ParseContext context = parseLabel(label.toString());
+                        Edge edge = new Edge();
+                        if (null != elements.get(edgeType.getSource())) {
+                            edge.setSourceVertex(elements.get(edgeType.getSource()));
+                        }
+                        if (null != elements.get(edgeType.getTarget())) {
+                            edge.setTargetVertex(elements.get(edgeType.getTarget()));
+                        }
+                        if (null != context.name()) {
+                            edge.setName(context.name().getText());
+                        }
+                        if (null != context.guard()) {
+                            edge.setGuard(new Guard(context.guard().getText()));
+                        }
+                        if (null != context.actions()) {
+                            edge.addActions(convert(context.actions().action()));
+                        }
+                        if (null != context.blocked()) {
+                            edge.setBlocked(true);
+                        }
+                        model.addEdge(edge);
                     }
-                    if (null != context.shared() && null != context.shared().Identifier()) {
-                        vertex.setSharedState(context.shared().Identifier().getText());
-                    }
-                    elements.put(id, vertex);
-                    return;
-                }
-            }
-        }
-    }
-
-    private void parseEdge(Edge edge, XMLStreamReader reader) throws XMLStreamException {
-        while (reader.hasNext()) {
-            switch (reader.next()) {
-                case START_ELEMENT: {
-                    if ("EdgeLabel".equals(reader.getLocalName()) && reader.hasNext()) {
-                        parseEdgeLabel(edge, reader);
-                    }
-                }
-                break;
-                case END_ELEMENT: {
-                    if ("edge".equals(reader.getLocalName())) {
-                        return;
-                    }
-                }
-                break;
-            }
-        }
-    }
-
-    private void parseEdgeLabel(Edge edge, XMLStreamReader reader) throws XMLStreamException {
-        StringBuilder builder = new StringBuilder();
-        while (reader.hasNext()) {
-            switch (reader.next()) {
-                case CHARACTERS: {
-                    builder.append(reader.getText());
-                }
-                break;
-                case END_ELEMENT: {
-                    LabelParser.ParseContext context = parseLabel(builder.toString());
-                    if (null != context.name()) {
-                        edge.setName(context.name().getText());
-                    }
-                    if (null != context.guard()) {
-                        edge.setGuard(new Guard(context.guard().getText()));
-                    }
-                    if (null != context.actions()) {
-                        edge.addActions(convert(context.actions().action()));
-                    }
-                    if (null != context.blocked()) {
-                        edge.setBlocked(true);
-                    }
-                    return;
                 }
             }
         }
@@ -193,4 +155,5 @@ public final class GraphMLModelFactory implements ModelFactory {
         LabelParser parser = new LabelParser(tokens);
         return parser.parse();
     }
+
 }
