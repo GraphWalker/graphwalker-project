b579876a2816d1ef9ccdd3dc029ec5a9f02de10c
Nils Olsson <nilols@spotify.com>
2014-05-21 09:08:25 +0200

    Split the LabelParser into VertexParser and EdgeParser

---
+++ src/main/antlr4/org/graphwalker/io/EdgeParser.g4
@@ -0,0 +1,40 @@
+parser grammar EdgeParser;
+
+options {
+	tokenVocab=LabelLexer;
+}
+
+parse
+ : name? guard? actions? blocked? EOF
+ | name? actions? blocked? guard? EOF
+ | name? blocked? guard? actions? EOF
+ | guard? actions? blocked? name? EOF
+ | guard? blocked? name? actions? EOF
+ | guard? name? actions? blocked? EOF
+ | actions? blocked? name? guard? EOF
+ | actions? name? guard? blocked? EOF
+ | actions? guard? blocked? name? EOF
+ | blocked? name? guard? actions? EOF
+ | blocked? guard? actions? name? EOF
+ | blocked? actions? name? guard? EOF
+ ;
+
+actions
+ : SLASH (action)+
+ ;
+
+action
+ : ~(SEMICOLON)* SEMICOLON
+ ;
+
+guard
+ : NestedBrackets
+ ;
+
+blocked
+ : BLOCKED
+ ;
+
+name
+ : Identifier
+ ;
--- src/test/java/org/graphwalker/io/label/LabelTest.java
+++ src/test/java/org/graphwalker/io/label/LabelTest.java
@@ -28,8 +28,9 @@ package org.graphwalker.io.label;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.graphwalker.io.EdgeParser;
 import org.graphwalker.io.LabelLexer;
-import org.graphwalker.io.LabelParser;
+import org.graphwalker.io.VertexParser;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -43,39 +44,70 @@ import static org.hamcrest.core.Is.is;
  */
 public class LabelTest {
 
-    private List<String> labels = Arrays.asList(
+
+    private List<String> vertexLabels = Arrays.asList(
             "",
-            "Start",
-            "v_BaseURL",
+            "Label",
+            "/* multi\nline\ncomment */                  Label",
+            "",
+            "v_vertex  \nSHARED:MyState",
+            "SHARED: MyState"
+    );
+
+    private List<String> edgeLabels = Arrays.asList(
+            "",
+            "Label",
             "e_AddBookToCart [num_of_books<=\nMAX_BOOKS] / num_of_books++; /* test */",
             "e_init / num_of_books = 0; MAX_BOOKS = 5;",
             "/* multi\nline\ncomment */ / i++; [i==0] name",
             "BLOCKED",
+            "BLOCKED e_AddBookToCart [num_of_books<=\nMAX_BOOKS] / num_of_books++; /* test */",
             "[ i ]",
-            "v_vertex  \nSHARED:MyState",
-            "SHARED: MyState",
             "e_Init/validLogin=false;rememberMe=false;"
     );
 
     @Test
-    public void testLabels() {
-        for (String label: labels) {
+    public void testVertexLabels() {
+        for (String label: vertexLabels) {
             ANTLRInputStream inputStream = new ANTLRInputStream(label);
             LabelLexer lexer = new LabelLexer(inputStream);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
-            LabelParser parser = new LabelParser(tokens);
-            LabelParser.ParseContext context = parser.parse();
+            VertexParser parser = new VertexParser(tokens);
+            VertexParser.ParseContext context = parser.parse();
             Assert.assertThat(parser.getNumberOfSyntaxErrors(), is(0));
         }
     }
 
+    @Test
+    public void testEdgeLabels() {
+        for (String label: edgeLabels) {
+            ANTLRInputStream inputStream = new ANTLRInputStream(label);
+            LabelLexer lexer = new LabelLexer(inputStream);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            EdgeParser parser = new EdgeParser(tokens);
+            EdgeParser.ParseContext context = parser.parse();
+            Assert.assertThat(parser.getNumberOfSyntaxErrors(), is(0));
+        }
+    }
+
+
     @Test(expected = AssertionError.class)
-    public void badLabel() {
+    public void badVertexLabel() {
         ANTLRInputStream inputStream = new ANTLRInputStream("1name");
         LabelLexer lexer = new LabelLexer(inputStream);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
-        LabelParser parser = new LabelParser(tokens);
-        LabelParser.ParseContext context = parser.parse();
+        VertexParser parser = new VertexParser(tokens);
+        VertexParser.ParseContext context = parser.parse();
+        Assert.assertThat(parser.getNumberOfSyntaxErrors(), is(0));
+    }
+
+    @Test(expected = AssertionError.class)
+    public void badEdgeLabel() {
+        ANTLRInputStream inputStream = new ANTLRInputStream("1name");
+        LabelLexer lexer = new LabelLexer(inputStream);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        EdgeParser parser = new EdgeParser(tokens);
+        EdgeParser.ParseContext context = parser.parse();
         Assert.assertThat(parser.getNumberOfSyntaxErrors(), is(0));
     }
 }
