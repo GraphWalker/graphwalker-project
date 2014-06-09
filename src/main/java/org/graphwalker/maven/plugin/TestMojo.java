8930c5aa4f14dd4d2010ec8892f291547e9c4537
Nils Olsson <nilols@spotify.com>
2014-06-09 09:18:38 +0200

    Add basic mojo structure

---
+++ src/main/java/org/graphwalker/maven/plugin/TestMojo.java
@@ -0,0 +1,166 @@
+package org.graphwalker.maven.plugin;
+
+/*
+ * #%L
+ * GraphWalker Maven Plugin
+ * %%
+ * Copyright (C) 2011 - 2014 GraphWalker
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.*;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * @author Nils Olsson
+ */
+@Mojo(name = "test", defaultPhase = LifecyclePhase.TEST, requiresDependencyResolution = ResolutionScope.TEST)
+@Execute(phase = LifecyclePhase.TEST_COMPILE, lifecycle = "graphwalker")
+public class TestMojo extends DefaultMojoBase {
+
+    @Parameter(property = "project.testClasspathElements")
+    private List<String> classpathElements;
+
+    @Parameter(defaultValue="${project.build.testOutputDirectory}")
+    private File testClassesDirectory;
+
+    @Parameter(defaultValue="${project.build.outputDirectory}")
+    private File classesDirectory;
+
+    @Parameter(defaultValue = "${project.build.directory}/graphwalker-reports")
+    private File reportsDirectory;
+
+    @Parameter(property = "maven.test.skip", defaultValue="false")
+    private boolean mavenTestSkip;
+
+    @Parameter(property = "skipTests", defaultValue = "false")
+    private boolean skipTests;
+
+    @Parameter(property = "graphwalker.test.skip", defaultValue = "false")
+    private boolean graphwalkerTestSkip;
+
+    @Parameter(property = "includes")
+    private Set<String> includes;
+
+    @Parameter(property = "excludes")
+    private Set<String> excludes;
+
+    @Parameter(property = "test", defaultValue = "*")
+    private String test;
+
+    @Parameter(property = "groups", defaultValue = "*")
+    private String groups;
+
+    protected List<String> getClasspathElements() {
+        return classpathElements;
+    }
+
+    protected File getTestClassesDirectory() {
+        return testClassesDirectory;
+    }
+
+    protected File getClassesDirectory() {
+        return classesDirectory;
+    }
+
+    protected File getReportsDirectory() {
+        return reportsDirectory;
+    }
+
+    protected boolean getSkipTests() {
+        return mavenTestSkip || graphwalkerTestSkip || skipTests;
+    }
+
+    protected Set<String> getIncludes() {
+        return includes;
+    }
+
+    protected Set<String> getExcludes() {
+        return excludes;
+    }
+
+    protected String getTest() {
+        if (System.getProperties().containsKey("test")) {
+            return System.getProperty("test");
+        }
+        return test;
+    }
+
+    protected String getGroups() {
+        if (System.getProperties().containsKey("groups")) {
+            return System.getProperty("groups");
+        }
+        return groups;
+    }
+
+    protected ClassLoader createClassLoader() throws MojoExecutionException {
+        try {
+            return new URLClassLoader(convertToURL(getClasspathElements()), getClass().getClassLoader());
+        } catch (MalformedURLException e) {
+            throw new MojoExecutionException("Couldn''t create class loader");
+        }
+    }
+
+    private URL[] convertToURL(List<String> elements) throws MalformedURLException {
+        List<URL> urlList = new ArrayList<>();
+        for (String element : elements) {
+            urlList.add(new File(element).toURI().toURL());
+        }
+        return urlList.toArray(new URL[urlList.size()]);
+    }
+
+    protected ClassLoader switchClassLoader(ClassLoader newClassLoader) {
+        ClassLoader oldClassLoader = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(newClassLoader);
+        return oldClassLoader;
+    }
+
+    protected Properties createProperties() {
+        Properties properties = (Properties) System.getProperties().clone();
+        properties.putAll((Properties) getMavenProject().getProperties().clone());
+        properties.put("groups", groups);
+        properties.put("test", test);
+        properties.putAll((Properties) getSession().getUserProperties().clone());
+        return properties;
+    }
+
+    protected Properties switchProperties(Properties properties) {
+        Properties oldProperties = (Properties) System.getProperties().clone();
+        System.setProperties(properties);
+        return oldProperties;
+    }
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        // TODO: Implement the execution of tests
+        getLog().info("TODO: Implement the execution of tests");
+    }
+}
