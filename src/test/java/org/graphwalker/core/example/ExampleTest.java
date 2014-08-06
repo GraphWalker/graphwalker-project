3fd0d38b2139a46bfbb1b4c7cc9066ab922c070a
Nils Olsson <nils.olsson@arcticblue.se>
2014-08-06 16:00:55 +0200

    Set FAILED as ExecutionStatus when an exception occurs during execution

--- src/test/java/org/graphwalker/core/example/ExampleTest.java
+++ src/test/java/org/graphwalker/core/example/ExampleTest.java
@@ -30,13 +30,17 @@ import org.graphwalker.core.condition.VertexCoverage;
 import org.graphwalker.core.generator.NoPathFoundException;
 import org.graphwalker.core.generator.RandomPath;
 import org.graphwalker.core.machine.ExecutionContext;
+import org.graphwalker.core.machine.ExecutionStatus;
 import org.graphwalker.core.machine.Machine;
 import org.graphwalker.core.machine.SimpleMachine;
 import org.graphwalker.core.model.Edge;
 import org.graphwalker.core.model.Guard;
 import org.graphwalker.core.model.Model;
 import org.graphwalker.core.model.Vertex;
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
 
 /**
  * @author Nils Olsson
@@ -53,6 +57,10 @@ public class ExampleTest extends ExecutionContext {
 
     public void vertex2() {
         System.out.println("vertex2");
+    }
+
+    public void vertex3() {
+        throw new RuntimeException();
     }
 
     public boolean isTrue() {
@@ -93,6 +101,31 @@ public class ExampleTest extends ExecutionContext {
         Machine machine = new SimpleMachine(this);
         while (machine.hasNextStep()) {
             machine.getNextStep();
+        }
+    }
+
+    @Test
+    public void exception() {
+        Vertex start = new Vertex();
+        Model model = new Model().addEdge(new Edge()
+                .setName("edge1")
+                .setGuard(new Guard("isTrue()"))
+                .setSourceVertex(start
+                        .setName("vertex3"))
+                .setTargetVertex(new Vertex()
+                        .setName("vertex2")));
+        this.setModel(model);
+        this.setPathGenerator(new RandomPath(new VertexCoverage(100)));
+        setNextElement(start);
+        Machine machine = new SimpleMachine(this);
+        Assert.assertThat(getExecutionStatus(), is(ExecutionStatus.NOT_EXECUTED));
+        try {
+            while (machine.hasNextStep()) {
+                machine.getNextStep();
+                Assert.assertThat(getExecutionStatus(), is(ExecutionStatus.EXECUTING));
+            }
+        } catch (Throwable t) {
+            Assert.assertThat(getExecutionStatus(), is(ExecutionStatus.FAILED));
         }
     }
 }
