6eb6e48ac0f599e2e1f8fb41507578eb1da6f87a
Kristian Karl <krikar@spotify.com>
2014-08-20 15:27:31 +0200

    Failing test for shortest all paths

--- src/test/java/org/graphwalker/core/machine/SimpleMachineTest.java
+++ src/test/java/org/graphwalker/core/machine/SimpleMachineTest.java
@@ -272,6 +272,48 @@ public class SimpleMachineTest {
         Assert.assertArrayEquals(expectedPath.toArray(), context.getProfiler().getPath().toArray());
     }
 
+
+    /**
+     * This is a programatic implementaion of the model:
+     * https://raw.githubusercontent.com/GraphWalker/graphwalker-cli/master/src/test/resources/graphml/shared_state/Login.graphml
+     */
+    @Test
+    public void loginShortestAllPathEdgeCoverage() {
+        Vertex start = new Vertex().setName("Start").setStartVertex(true);
+        Vertex v_Browse = new Vertex().setName("v_Browse").setSharedState("LOGGED_IN");
+        Vertex v_ClientNotRunning = new Vertex().setName("v_ClientNotRunning").setSharedState("CLIENT_NOT_RUNNING");
+        Vertex v_LoginPrompted = new Vertex().setName("v_LoginPrompted");
+
+        Edge e_Close = new Edge().setName("e_Close").setSourceVertex(v_LoginPrompted).setTargetVertex(v_ClientNotRunning);
+        Edge e_Exit = new Edge().setName("e_Exit").setSourceVertex(v_Browse).setTargetVertex(v_ClientNotRunning);
+        Edge e_Init = new Edge().setName("e_Init").setSourceVertex(start).setTargetVertex(v_ClientNotRunning).addAction(new Action("validLogin=false")).addAction(new Action("rememberMe=false"));
+        Edge e_InvalidCredentials = new Edge().setName("e_InvalidCredentials").setSourceVertex(v_LoginPrompted).setTargetVertex(v_LoginPrompted).addAction(new Action("validLogin=false"));
+        Edge e_Logout = new Edge().setName("e_Logout").setSourceVertex(v_Browse).setTargetVertex(v_LoginPrompted);
+        Edge e_StartClient_1 = new Edge().setName("e_StartClient").setSourceVertex(v_ClientNotRunning).setTargetVertex(v_LoginPrompted).setGuard(new Guard("!rememberMe||!validLogin"));
+        Edge e_StartClient_2 = new Edge().setName("e_StartClient").setSourceVertex(v_ClientNotRunning).setTargetVertex(v_Browse).setGuard(new Guard("rememberMe&&validLogin"));
+        Edge e_ToggleRememberMe = new Edge().setName("e_ToggleRememberMe").setSourceVertex(v_LoginPrompted).setTargetVertex(v_LoginPrompted).addAction(new Action("rememberMe=true"));
+        Edge e_ValidPremiumCredentials = new Edge().setName("e_ValidPremiumCredentials").setSourceVertex(v_LoginPrompted).setTargetVertex(v_Browse).addAction(new Action("validLogin=true"));
+
+        Model model = new Model().addEdge(e_Close).
+            addEdge(e_Exit).
+            addEdge(e_Init).
+            addEdge(e_InvalidCredentials).
+            addEdge(e_Logout).
+            addEdge(e_StartClient_1).
+            addEdge(e_StartClient_2).
+            addEdge(e_ToggleRememberMe).
+            addEdge(e_ValidPremiumCredentials);
+
+        ExecutionContext context = new ExecutionContext(model, new ShortestAllPaths(new EdgeCoverage(100)));
+        context.setNextElement(start);
+        Machine machine = new SimpleMachine(context);
+
+        while (machine.hasNextStep()) {
+            machine.getNextStep();
+            System.out.println(context.getCurrentElement().getName());
+        }
+    }
+
     @Test
     public void simpleAStar() {
         Vertex start = new Vertex().setName("Start");
