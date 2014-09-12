a012907ca281f181850fcd8363ea0509385ed017
Kristian Karl <krikar@spotify.com>
2014-09-12 16:50:03 +0200

    Add name to model

--- src/main/java/org/graphwalker/io/factory/dot/DotContextFactory.java
+++ src/main/java/org/graphwalker/io/factory/dot/DotContextFactory.java
@@ -124,6 +124,7 @@ public final class DotContextFactory implements ContextFactory {
             model.addEdge(edge);
         }
 
+        model.setName(path.toString());
         context.setModel(model.build());
         if (null != startEdge) {
             context.setNextElement(startEdge);
