c014701ba9d6fc17458f01e956180c03da113445
Kristian Karl <kristian.hermann.karl@gmail.com>
2014-09-13 11:53:07 +0200

    Added JSON output
    Also refactored duplicated code into a utility class.

--- src/main/java/org/graphwalker/cli/CLI.java
+++ src/main/java/org/graphwalker/cli/CLI.java
@@ -241,31 +241,11 @@ public class CLI {
         while (machine.hasNextStep()) {
             try {
                 machine.getNextStep();
-                if (offline.verbose) {
-                    System.out.print(FilenameUtils.getBaseName(machine.getCurrentContext().getModel().getName()) + " : ");
+                if (online.json) {
+                    System.out.println(Util.getStepAsJSON(machine, offline.verbose, offline.unvisited).toString());
+                } else {
+                    System.out.println(Util.getStepAsString(machine, offline.verbose, offline.unvisited));
                 }
-                if (machine.getCurrentContext().getCurrentElement().hasName()) {
-                    System.out.print(machine.getCurrentContext().getCurrentElement().getName());
-                    if (offline.verbose) {
-                        System.out.print("(" + machine.getCurrentContext().getCurrentElement().getId() + ")");
-                        System.out.print(":" + machine.getCurrentContext().getKeys());
-                    }
-                }
-
-                if (offline.unvisited) {
-                    System.out.print(" | " + machine.getCurrentContext().getProfiler().getUnvisitedElements().size() +
-                        "(" + machine.getCurrentContext().getModel().getElements().size() + ") : ");
-
-                    for (Element e : machine.getCurrentContext().getProfiler().getUnvisitedElements()) {
-                        System.out.print(e.getName());
-                        if (offline.verbose) {
-                            System.out.print("(" + e.getId() + ")");
-                        }
-                        System.out.print(" ");
-                    }
-                }
-
-                System.out.println();
             } catch (MachineException e) {
                 throw e;
             }
