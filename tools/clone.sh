062ea2bc68f1df31bdbeb404d6ce70b9115a7909
Kristian Karl <krikar@spotify.com>
2014-09-15 11:23:25 +0200

    Added maven-archetype

--- tools/clone.sh
+++ tools/clone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-modules="core java io cli maven-plugin gradle-plugin dashboard example"
+modules="core java io cli maven-plugin gradle-plugin dashboard example maven-archetype"
 for module in $modules
 do
     pushd ..
