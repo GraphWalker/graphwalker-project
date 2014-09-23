40a92f83863fe424395faabdcfbdceaa7cba179b
Kristian Karl <krikar@spotify.com>
2014-09-23 14:49:22 +0200

    Added dsl module

--- tools/clone.sh
+++ tools/clone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-modules="core java io cli maven-plugin gradle-plugin dashboard example maven-archetype"
+modules="core java io cli dsl maven-plugin gradle-plugin dashboard example maven-archetype"
 for module in $modules
 do
     pushd ..
