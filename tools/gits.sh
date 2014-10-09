e9a094c31d60ef87c0164c6dc7ea309faf15650e
Kristian Karl <krikar@spotify.com>
2014-10-09 13:25:32 +0200

    Added project module

--- tools/gits.sh
+++ tools/gits.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-
-
 set -ev
-modules="core java io cli dsl maven-plugin gradle-plugin dashboard example maven-archetype"
+modules="project core java io cli dsl maven-plugin gradle-plugin dashboard example maven-archetype"
 for module in $modules
 do
     pushd ..
