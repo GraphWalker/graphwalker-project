dfde8825b5676179e9920e7075c52df2bb7a2889
Nils Olsson <nilols@spotify.com>
2014-12-17 11:38:52 +0100

    Add test for manipulate model variables

---
+++ graphwalker-core/src/test/java/org/graphwalker/core/machine/AccessModelTest.java
@@ -0,0 +1,41 @@
+package org.graphwalker.core.machine;
+
+import org.graphwalker.core.condition.VertexCoverage;
+import org.graphwalker.core.generator.ShortestAllPaths;
+import org.graphwalker.core.model.Action;
+import org.graphwalker.core.model.Edge;
+import org.graphwalker.core.model.Model;
+import org.graphwalker.core.model.Vertex;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+
+/**
+ * @author Nils Olsson
+ */
+public class AccessModelTest {
+
+    @Test
+    public void read() {
+        ExecutionContext context = createContext();
+        Assert.assertThat((Double)context.getAttribute("x"), is(1.0));
+    }
+
+    @Test
+    public void write() {
+        ExecutionContext context = createContext();
+        context.setAttribute("y", 2);
+        Assert.assertThat((Integer)context.getAttribute("y"), is(2));
+    }
+
+    private ExecutionContext createContext() {
+        Model model = new Model();
+        model.addEdge(new Edge()
+                .setSourceVertex(new Vertex())
+                .setTargetVertex(new Vertex()));
+        ExecutionContext context = new TestExecutionContext(model, new ShortestAllPaths(new VertexCoverage(100)));
+        context.execute(new Action("x = 1;"));
+        return context;
+    }
+}
