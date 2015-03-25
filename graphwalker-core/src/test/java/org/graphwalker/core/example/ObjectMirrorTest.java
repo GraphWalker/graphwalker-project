b58141cf29518f0c7fd06603628c8f17c0a16657
Nils Olsson <nils.olsson@arcticblue.se>
2015-03-25 15:21:53 +0100

    Add support for mirroring methods with arguments

---
+++ graphwalker-core/src/test/java/org/graphwalker/core/example/ObjectMirrorTest.java
@@ -0,0 +1,54 @@
+package org.graphwalker.core.example;
+
+import org.graphwalker.core.condition.VertexCoverage;
+import org.graphwalker.core.generator.RandomPath;
+import org.graphwalker.core.machine.ExecutionContext;
+import org.graphwalker.core.machine.Machine;
+import org.graphwalker.core.machine.SimpleMachine;
+import org.graphwalker.core.model.Action;
+import org.graphwalker.core.model.Edge;
+import org.graphwalker.core.model.Guard;
+import org.graphwalker.core.model.Model;
+import org.graphwalker.core.model.Vertex;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Nils Olsson
+ */
+public class ObjectMirrorTest extends ExecutionContext {
+
+	private List<String> values = new ArrayList<>();
+
+	public boolean contains(String value) {
+		return values.contains(value);
+	}
+
+	public void add(String value) {
+		values.add(value);
+	}
+
+	@Test
+	public void verifyMirror() {
+		Vertex start = new Vertex();
+		Model model = new Model()
+				.addEdge(new Edge().setName("edge1")
+						.setGuard(new Guard("contains('value1')"))
+						.setSourceVertex(start.setName("vertex1"))
+						.setTargetVertex(new Vertex().setName("vertex2")))
+				.addEdge(new Edge()
+						.setGuard(new Guard("!contains('value1')"))
+						.setSourceVertex(start)
+						.setTargetVertex(start)
+						.addAction(new Action("add('value1')")));
+		this.setModel(model.build());
+		this.setPathGenerator(new RandomPath(new VertexCoverage(100)));
+		setNextElement(start);
+		Machine machine = new SimpleMachine(this);
+		while (machine.hasNextStep()) {
+			machine.getNextStep();
+		}
+	}
+}
