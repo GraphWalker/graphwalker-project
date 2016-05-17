becf40f11271b3403a8b0ed39ea446aabc17a8e1
Kristian Karl <kristian.hermann.karl@gmail.com>
2016-05-17 12:48:45 +0200

    Removed import that is not used

--- graphwalker-core/src/main/java/org/graphwalker/core/statistics/Profiler.java
+++ graphwalker-core/src/main/java/org/graphwalker/core/statistics/Profiler.java
@@ -39,7 +39,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.zip.ZipFile;
 
 import static org.graphwalker.core.common.Objects.isNotNull;
 
