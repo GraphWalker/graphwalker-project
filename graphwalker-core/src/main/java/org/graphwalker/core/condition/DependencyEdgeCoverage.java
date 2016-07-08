4ca5b7dd9885e21ba57b8bfb09b8e8b8b8e674a0
Miroslav Janeski <mikijaneski@gmail.com>
2016-07-08 07:41:16 +0200

    Added Dependency Stop COndition

---
+++ graphwalker-core/src/main/java/org/graphwalker/core/condition/DependencyEdgeCoverage.java
@@ -0,0 +1,70 @@
+package org.graphwalker.core.condition;
+
+/*
+ * #%L
+ * GraphWalker Core
+ * %%
+ * Copyright (C) 2005 - 2014 GraphWalker
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+import org.graphwalker.core.machine.Context;
+
+import static org.graphwalker.core.model.Edge.RuntimeEdge;
+
+/**
+ * <h1>DependencyEdgeCoverage</h1> The EdgeCoverage stop condition is fulfilled
+ * when the percentage of visited edges in the model is greater than, or equal,
+ * to the percentage given as a parameter to the constructor.
+ * </p>
+ *
+ * @author Miroslav Janeski
+ */
+public final class DependencyEdgeCoverage extends DependencyCoverageStopConditionBase {
+
+	public DependencyEdgeCoverage(int percent, int dependency) {
+		super(percent, dependency);
+	}
+
+	@Override
+	public boolean isFulfilled() {
+		boolean temp = super.isFulfilled();
+		return getFulfilment() >= super.getPercentAsDouble() && super.isFulfilled();
+	}
+
+	@Override
+	public double getFulfilment() {
+		Context context = getContext();
+
+		long totalEdgesCount = 0;
+		long visitedEdgesCount = 0;
+		for (RuntimeEdge edge : context.getModel().getEdges()) {
+			if (edge.getDependency() >= super.getDependencyAsDouble()) {
+				totalEdgesCount++;
+			}
+			if (context.getProfiler().isVisited(edge) && edge.getDependency() >= super.getDependencyAsDouble()) {
+				visitedEdgesCount++;
+			}
+		}
+
+		return ((double) visitedEdgesCount / totalEdgesCount) / getPercentAsDouble();
+	}
+}
--- graphwalker-core/src/main/java/org/graphwalker/core/model/Edge.java
+++ graphwalker-core/src/main/java/org/graphwalker/core/model/Edge.java
@@ -39,281 +39,304 @@ import static org.graphwalker.core.common.Objects.unmodifiableList;
 import static org.graphwalker.core.model.Vertex.RuntimeVertex;
 
 /**
- * <h1>Edge</h1>
- * The  Edge holds the information for a transition in a model.
+ * <h1>Edge</h1> The Edge holds the information for a transition in a model.
  * </p>
  * The edge represents an action performed by a test, which takes the system
- * under test, from a state to another.
- * The edge has a source and target vertex. If the vertices are identical, the
- * edge is a self loop. The source vertex is not mandatory, but in a model,
- * there should be only one such instance. Also, the target vertex is not
- * mandatory, but again, in a model, there should be only one such instance.
+ * under test, from a state to another. The edge has a source and target vertex.
+ * If the vertices are identical, the edge is a self loop. The source vertex is
+ * not mandatory, but in a model, there should be only one such instance. Also,
+ * the target vertex is not mandatory, but again, in a model, there should be
+ * only one such instance.
  *
  * @author Nils Olsson
  */
 public final class Edge extends CachedBuilder<Edge, Edge.RuntimeEdge> {
 
-  private Vertex sourceVertex;
-  private Vertex targetVertex;
-  private Guard guard;
-  private List<Action> actions = new ArrayList<>();
-  private Double weight = 0.0;
+	private Vertex sourceVertex;
+	private Vertex targetVertex;
+	private Guard guard;
+	private List<Action> actions = new ArrayList<>();
+	private Double weight = 0.0;
+	private Double dependency = 0.0;
 
-  /**
-   * Sets the source vertex of the edge.
-   *
-   * @param vertex The source vertex.
-   * @return The edge.
-   */
-  public Edge setSourceVertex(Vertex vertex) {
-    this.sourceVertex = vertex;
-    invalidateCache();
-    return this;
-  }
+	/**
+	 * Sets the source vertex of the edge.
+	 *
+	 * @param vertex
+	 *            The source vertex.
+	 * @return The edge.
+	 */
+	public Edge setSourceVertex(Vertex vertex) {
+		this.sourceVertex = vertex;
+		invalidateCache();
+		return this;
+	}
 
-  /**
-   * Gets the source vertex.
-   *
-   * @return The source vertex.
-   * @see Edge#setSourceVertex
-   */
-  public Vertex getSourceVertex() {
-    return sourceVertex;
-  }
+	/**
+	 * Gets the source vertex.
+	 *
+	 * @return The source vertex.
+	 * @see Edge#setSourceVertex
+	 */
+	public Vertex getSourceVertex() {
+		return sourceVertex;
+	}
 
-  /**
-   * Sets the target vertex of the edge.
-   *
-   * @param vertex The target vertex.
-   * @return The edge.
-   */
-  public Edge setTargetVertex(Vertex vertex) {
-    this.targetVertex = vertex;
-    invalidateCache();
-    return this;
-  }
+	/**
+	 * Sets the target vertex of the edge.
+	 *
+	 * @param vertex
+	 *            The target vertex.
+	 * @return The edge.
+	 */
+	public Edge setTargetVertex(Vertex vertex) {
+		this.targetVertex = vertex;
+		invalidateCache();
+		return this;
+	}
 
-  /**
-   * Gets the target vertex of the edge.
-   *
-   * @return The vertex.
-   * @see Edge#setTargetVertex
-   */
-  public Vertex getTargetVertex() {
-    return targetVertex;
-  }
+	/**
+	 * Gets the target vertex of the edge.
+	 *
+	 * @return The vertex.
+	 * @see Edge#setTargetVertex
+	 */
+	public Vertex getTargetVertex() {
+		return targetVertex;
+	}
 
-  /**
-   * Sets the guard of the edge. The code in the guard is by default interpreted as javascript.
-   * The guard works like an 'if-statement'. It controls the accessibility of the edge.
-   * During execution, the guard evaluates to a boolean expression.
-   * If true, the edge is accessible, else it's not.
-   *
-   * @param guard The guard.
-   * @return The edge.
-   */
-  public Edge setGuard(Guard guard) {
-    this.guard = guard;
-    invalidateCache();
-    return this;
-  }
+	/**
+	 * Sets the guard of the edge. The code in the guard is by default
+	 * interpreted as javascript. The guard works like an 'if-statement'. It
+	 * controls the accessibility of the edge. During execution, the guard
+	 * evaluates to a boolean expression. If true, the edge is accessible, else
+	 * it's not.
+	 *
+	 * @param guard
+	 *            The guard.
+	 * @return The edge.
+	 */
+	public Edge setGuard(Guard guard) {
+		this.guard = guard;
+		invalidateCache();
+		return this;
+	}
 
-  /**
-   * Gets the guard of the edge.
-   *
-   * @return The guard.
-   * @see Edge#setGuard
-   */
-  public Guard getGuard() {
-    return guard;
-  }
+	/**
+	 * Gets the guard of the edge.
+	 *
+	 * @return The guard.
+	 * @see Edge#setGuard
+	 */
+	public Guard getGuard() {
+		return guard;
+	}
 
-  /**
-   * Adds an action to the edge, which represents a piece of code that will be executed
-   * each time the edge is being traversed. The code is by default interpreted as javascript.
-   *
-   * @param action The action.
-   * @return The edge.
-   */
-  public Edge addAction(Action action) {
-    this.actions.add(action);
-    invalidateCache();
-    return this;
-  }
+	/**
+	 * Adds an action to the edge, which represents a piece of code that will be
+	 * executed each time the edge is being traversed. The code is by default
+	 * interpreted as javascript.
+	 *
+	 * @param action
+	 *            The action.
+	 * @return The edge.
+	 */
+	public Edge addAction(Action action) {
+		this.actions.add(action);
+		invalidateCache();
+		return this;
+	}
 
-  public Edge addActions(Action... actions) {
-    return addActions(Arrays.asList(actions));
-  }
+	public Edge addActions(Action... actions) {
+		return addActions(Arrays.asList(actions));
+	}
 
-  public Edge addActions(List<Action> actions) {
-    this.actions.addAll(actions);
-    return this;
-  }
+	public Edge addActions(List<Action> actions) {
+		this.actions.addAll(actions);
+		return this;
+	}
 
-  /**
-   * Adds a list of actions to the edge, which represents a pieces of code that will be executed
-   * each time the edge is being traversed. The code snippets is by default interpreted as javascript.
-   *
-   * @param actions The actions.
-   * @return The edge.
-   * @see Edge#addAction
-   */
-  public Edge setActions(List<Action> actions) {
-    this.actions = new ArrayList<>(actions);
-    invalidateCache();
-    return this;
-  }
+	/**
+	 * Adds a list of actions to the edge, which represents a pieces of code
+	 * that will be executed each time the edge is being traversed. The code
+	 * snippets is by default interpreted as javascript.
+	 *
+	 * @param actions
+	 *            The actions.
+	 * @return The edge.
+	 * @see Edge#addAction
+	 */
+	public Edge setActions(List<Action> actions) {
+		this.actions = new ArrayList<>(actions);
+		invalidateCache();
+		return this;
+	}
 
-  /**
-   * Gets the lists of actions of the edge.
-   *
-   * @return The actions
-   * @see Edge#setActions
-   */
-  public List<Action> getActions() {
-    return unmodifiableList(actions);
-  }
+	/**
+	 * Gets the lists of actions of the edge.
+	 *
+	 * @return The actions
+	 * @see Edge#setActions
+	 */
+	public List<Action> getActions() {
+		return unmodifiableList(actions);
+	}
 
-  /**
-   * Gets the weight of the edge.
-   *
-   * @return The weight as double.
-   * @see Edge#setWeight
-   */
-  public Double getWeight() {
-    return weight;
-  }
+	/**
+	 * Gets the weight of the edge.
+	 *
+	 * @return The weight as double.
+	 * @see Edge#setWeight
+	 */
+	public Double getWeight() {
+		return weight;
+	}
 
-  /**
-   * The weight is used as probability when using the {@link org.graphwalker.core.generator.WeightedRandomPath}.
-   * Weight means the probability for the edge to be selected.
-   *
-   * @param weight a double between 0 and 1
-   * @return The edge
-   */
-  public Edge setWeight(Double weight) {
-    this.weight = weight;
-    invalidateCache();
-    return this;
-  }
+	/**
+	 * The weight is used as probability when using the
+	 * {@link org.graphwalker.core.generator.WeightedRandomPath}. Weight means
+	 * the probability for the edge to be selected.
+	 *
+	 * @param weight
+	 *            a double between 0 and 1
+	 * @return The edge
+	 */
+	public Edge setWeight(Double weight) {
+		this.weight = weight;
+		invalidateCache();
+		return this;
+	}
 
-  /**
-   * Creates an immutable edge from this edge.
-   *
-   * @return An immutable edge as a RuntimeEdge
-   */
-  @Override
-  protected RuntimeEdge createCache() {
-    return new RuntimeEdge(this);
-  }
+	/**
+	 * Creates an immutable edge from this edge.
+	 *
+	 * @return An immutable edge as a RuntimeEdge
+	 */
+	@Override
+	protected RuntimeEdge createCache() {
+		return new RuntimeEdge(this);
+	}
 
-  /**
-   * <h1>RuntimeEdge</h1>
-   * Immutable class for Edge
-   * </p>
-   * This class is used in models. It guarantees that that the internal states of
-   * the instance will not change after it's construction.
-   * </p>
-   */
-  public static final class RuntimeEdge extends RuntimeBase {
+	public Double getDependency() {
+		return dependency;
+	}
 
-    private final RuntimeVertex sourceVertex;
-    private final RuntimeVertex targetVertex;
-    private final Guard guard;
-    private final Double weight;
+	public Edge setDependency(Double dependency) {
+		this.dependency = dependency;
+		invalidateCache();
+		return this;
+	}
 
-    private RuntimeEdge(Edge edge) {
-      super(edge.getId(), edge.getName(), edge.getActions(), edge.getRequirements(), edge.getProperties());
-      this.sourceVertex = build(edge.getSourceVertex());
-      this.targetVertex = build(edge.getTargetVertex());
-      this.guard = edge.getGuard();
-      this.weight = edge.getWeight();
-    }
+	/**
+	 * <h1>RuntimeEdge</h1> Immutable class for Edge
+	 * </p>
+	 * This class is used in models. It guarantees that that the internal states
+	 * of the instance will not change after it's construction.
+	 * </p>
+	 */
+	public static final class RuntimeEdge extends RuntimeBase {
 
-    private <T> T build(Builder<T> builder) {
-      return isNotNull(builder) ? builder.build() : null;
-    }
+		private final RuntimeVertex sourceVertex;
+		private final RuntimeVertex targetVertex;
+		private final Guard guard;
+		private final Double weight;
+        private final Double dependency;
+        
+		private RuntimeEdge(Edge edge) {
+			super(edge.getId(), edge.getName(), edge.getActions(), edge.getRequirements(), edge.getProperties());
+			this.sourceVertex = build(edge.getSourceVertex());
+			this.targetVertex = build(edge.getTargetVertex());
+			this.guard = edge.getGuard();
+			this.weight = edge.getWeight();
+			this.dependency = edge.getDependency();
+		}
 
-    /**
-     * Gets the source vertex.
-     *
-     * @return The source vertex.
-     * @see Edge#setSourceVertex
-     */
-    public RuntimeVertex getSourceVertex() {
-      return sourceVertex;
-    }
+		private <T> T build(Builder<T> builder) {
+			return isNotNull(builder) ? builder.build() : null;
+		}
 
-    /**
-     * Gets the target vertex of the edge.
-     *
-     * @return The vertex.
-     * @see Edge#setTargetVertex
-     */
-    public RuntimeVertex getTargetVertex() {
-      return targetVertex;
-    }
+		/**
+		 * Gets the source vertex.
+		 *
+		 * @return The source vertex.
+		 * @see Edge#setSourceVertex
+		 */
+		public RuntimeVertex getSourceVertex() {
+			return sourceVertex;
+		}
 
-    /**
-     * Gets the guard of the edge.
-     *
-     * @return The guard.
-     * @see Edge#setGuard
-     */
-    public Guard getGuard() {
-      return guard;
-    }
+		/**
+		 * Gets the target vertex of the edge.
+		 *
+		 * @return The vertex.
+		 * @see Edge#setTargetVertex
+		 */
+		public RuntimeVertex getTargetVertex() {
+			return targetVertex;
+		}
 
-    public boolean hasGuard() {
-      return isNotNull(guard) && isNotNullOrEmpty(guard.getScript());
-    }
+		/**
+		 * Gets the guard of the edge.
+		 *
+		 * @return The guard.
+		 * @see Edge#setGuard
+		 */
+		public Guard getGuard() {
+			return guard;
+		}
 
-    /**
-     * Gets the weight of the edge.
-     *
-     * @return The weight as double.
-     * @see Edge#setWeight
-     */
-    public Double getWeight() {
-      return weight;
-    }
+		public boolean hasGuard() {
+			return isNotNull(guard) && isNotNullOrEmpty(guard.getScript());
+		}
 
-    /**
-     * TODO Needs documentation
-     *
-     * @param visitor 
-     */
-    @Override
-    public void accept(ElementVisitor visitor) {
-      visitor.visit(this);
-    }
+		/**
+		 * Gets the weight of the edge.
+		 *
+		 * @return The weight as double.
+		 * @see Edge#setWeight
+		 */
+		public Double getWeight() {
+			return weight;
+		}
+		
+		public Double getDependency() {
+			return dependency;
+		}
 
-    @Override
-    public int hashCode() {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + ((guard == null) ? 0 : guard.hashCode());
-      result = prime * result
-        + ((sourceVertex == null) ? 0 : sourceVertex.hashCode());
-      result = prime * result
-        + ((targetVertex == null) ? 0 : targetVertex.hashCode());
-      result = prime * result
-        + ((weight == null) ? 0 : weight.hashCode());
-      return result;
-    }
+		/**
+		 * TODO Needs documentation
+		 *
+		 * @param visitor
+		 */
+		@Override
+		public void accept(ElementVisitor visitor) {
+			visitor.visit(this);
+		}
 
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) return true;
-      if (isNull(o) || getClass() != o.getClass()) return false;
-      if (!super.equals(o)) return false;
-      RuntimeEdge that = (RuntimeEdge) o;
-      return Objects.equals(sourceVertex, that.sourceVertex) &&
-        Objects.equals(targetVertex, that.targetVertex) &&
-        Objects.equals(guard, that.guard) &&
-        Objects.equals(weight, that.weight);
-    }
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((guard == null) ? 0 : guard.hashCode());
+			result = prime * result + ((sourceVertex == null) ? 0 : sourceVertex.hashCode());
+			result = prime * result + ((targetVertex == null) ? 0 : targetVertex.hashCode());
+			result = prime * result + ((weight == null) ? 0 : weight.hashCode());
+			result = prime * result + ((dependency == null) ? 0 : dependency.hashCode());
+			return result;
+		}
 
+		@Override
+		public boolean equals(Object o) {
+			if (this == o)
+				return true;
+			if (isNull(o) || getClass() != o.getClass())
+				return false;
+			if (!super.equals(o))
+				return false;
+			RuntimeEdge that = (RuntimeEdge) o;
+			return Objects.equals(sourceVertex, that.sourceVertex) && Objects.equals(targetVertex, that.targetVertex)
+					&& Objects.equals(guard, that.guard) && Objects.equals(weight, that.weight) && Objects.equals(dependency, that.dependency);
+		}
 
-  }
+	}
 }
