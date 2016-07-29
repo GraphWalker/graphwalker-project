8943add1e38893e91938651f3d1e7ce4f8fef0ec
Kristian Karl <kristian.hermann.karl@gmail.com>
2016-07-29 18:49:42 +0200

    Added command line options for Studio

---
+++ graphwalker-studio/src/main/java/org/graphwalker/studio/Options.java
@@ -0,0 +1,47 @@
+package org.graphwalker.studio;
+
+/*
+ * #%L
+ * GraphWalker Command Line Interface
+ * %%
+ * Copyright (C) 2005 - 2014 GraphWalker
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+import com.beust.jcommander.Parameter;
+
+public class Options {
+
+  @Parameter(names = {"--help", "-h"}, description = "Prints help text")
+  public boolean help = false;
+
+  @Parameter(names = {"--version", "-v"}, description = "Prints the version of graphwalker")
+  public boolean version = false;
+
+  @Parameter(names = {"--websocket-port", "-w"}, description = "Sets the port of the websocket service")
+  public int wsPort = 9999;
+
+  @Parameter(names = {"--browser-port", "-b"}, description = "Sets the port of the web server service")
+  public int browserPort = 9090;
+
+  @Parameter(names = {"--debug", "-d"}, description = "Sets the log level: OFF, ERROR, WARN, INFO, DEBUG, TRACE, ALL. Default is OFF")
+  public String debug = "OFF";
+}
