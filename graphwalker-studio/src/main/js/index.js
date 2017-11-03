9e060ee8d3348d87e5e3d701c9988d031828653a
Nils Olsson <nils.olsson@arcticblue.se>
2017-11-03 20:21:01 +0100

    Update how the Roboto font is loaded

--- graphwalker-studio/src/main/js/index.js
+++ graphwalker-studio/src/main/js/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Studio from './views/studio';
+import 'typeface-roboto';
 import './styles.less';
 
 ReactDOM.render(<Studio />, document.getElementById('studio'));
