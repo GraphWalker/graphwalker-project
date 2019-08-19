7fb5994b7db1d08f4a7eb6d128e5cad6507a4b05
Nils Olsson <nils.olsson@arcticblue.se>
2019-08-19 19:33:32 +0200

    Implement context menu actions

--- graphwalker-studio/src/main/js/components/editor/editor-panel/editor-component.js
+++ graphwalker-studio/src/main/js/components/editor/editor-panel/editor-component.js
@@ -2,11 +2,15 @@ import React, { Component, createElement } from 'react';
 import { findDOMNode } from "react-dom";
 import { connect } from "react-redux";
 import { selectElement } from "../../../redux/actions";
-import { Classes, ContextMenu, Menu, MenuDivider, MenuItem, ResizeSensor } from "@blueprintjs/core";
+import {Classes, ContextMenu, Divider, Menu, MenuDivider, MenuItem, ResizeSensor} from "@blueprintjs/core";
 import { debounce } from "debounce";
 import Cytoscape from "cytoscape";
+import coseBilkent from 'cytoscape-cose-bilkent';
 import stylesheet from "./editor-stylesheet";
+
 import "./style.css";
+
+Cytoscape.use( coseBilkent );
 
 class EditorComponent extends Component {
 
@@ -37,16 +41,16 @@ class EditorComponent extends Component {
       }
     });
 
-
-    this.editor.on('cxttap', 'node', event => {
+    this.editor.on('cxttap', 'node, edge', event => {
       const { clientX, clientY } = event.originalEvent;
       ContextMenu.show(
         <Menu>
+          <MenuItem icon="cross" text="Delete" onClick={() => event.target.remove()}/>
+          <Divider/>
           <MenuItem icon="full-circle" text="Breakpoint...">
-            <MenuItem icon="new-object" text="Add breakpoint" />
-            <MenuItem icon="graph-remove" text="Remove breakpoint" />
+            <MenuItem disabled={true} icon="new-object" text="Add breakpoint" />
+            <MenuItem disabled={true} icon="graph-remove" text="Remove breakpoint" />
           </MenuItem>
-          <MenuItem icon="cross" text="Delete" />
         </Menu>, { left: clientX, top: clientY });
     })
 
@@ -55,17 +59,18 @@ class EditorComponent extends Component {
         const { clientX, clientY } = event.originalEvent;
         ContextMenu.show(
           <Menu>
-            <MenuItem icon="insert" text="Add vertex" />
-            <MenuItem icon="select" text="Select all" />
+            <MenuItem disabled={true} icon="insert" text="Add vertex" />
+            <MenuItem icon="select" text="Select all" onClick={() => this.editor.elements().select()} />
+            <MenuItem disabled={this.editor.elements(':selected').length === 0} icon="cross" text="Delete selected" onClick={() => this.editor.remove(':selected')}/>
+            <Divider/>
             <MenuItem icon="layout" text="Layout...">
-              <MenuItem icon="layout-auto" text="Auto" />
-              <MenuItem icon="layout-circle" text="Circle" />
-              <MenuItem icon="layout-grid" text="Grid" />
+              <MenuItem icon="layout-auto" text="Auto" onClick={() => this.editor.layout({ name: 'cose-bilkent', nodeDimensionsIncludeLabels: true, idealEdgeLength: 200 }).run()} />
+              <MenuItem icon="layout-circle" text="Circle" onClick={() => this.editor.layout({ name: 'circle' }).run()} />
+              <MenuItem icon="layout-grid" text="Grid" onClick={() => this.editor.layout({ name: 'grid' }).run()} />
             </MenuItem>
           </Menu>, { left: clientX, top: clientY });
       }
     });
-
   }
 
   handleResize = debounce(() => {
